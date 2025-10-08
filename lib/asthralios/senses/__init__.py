cf7f0f8f1e6449e09a65e29e8bb2eded2d0d05b8
Markizano Draconus <markizano@markizano.net>
2025-10-08 08:53:47 -0500

    enabling barrel import for senses.

---
+++ lib/asthralios/senses/__init__.py
@@ -0,0 +1,7 @@
+import asthralios.senses.ears as ears
+import asthralios.senses.hands as hands
+
+conversate = ears.conversate
+ingest = hands.ingest
+
+__all__ = ['ears', 'hands', 'conversate', 'ingest']

d0fb7791c3120adebbfee9e1a83da43099acd4e4
Markizano Draconus <markizano@markizano.net>
2025-10-08 08:52:08 -0500

    modularizing house a bit. Enabling barrel `import asthralios` and go instead of having to import all submodules too.

--- lib/asthralios/__init__.py
+++ lib/asthralios/__init__.py
@@ -1,3 +1,7 @@
 from kizano import getLogger
 import asthralios.config as config
+import asthralios.senses as senses
 import asthralios.chat as chat
+import asthralios.sentinel as sentinel
+
+__all__ = ['config', 'senses', 'chat', 'sentinel']
